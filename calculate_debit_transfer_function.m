fd75f536baebddcdc225e5ddfb7b505807077a96
= <danb.alexandru@gmail.com>
2019-12-07 19:29:27 +0200

    Added the functionality of the 'calculate_debit_transfer_function'

--- calculate_debit_transfer_function.m
+++ calculate_debit_transfer_function.m
@@ -1,5 +1,24 @@
 %% Description
 % This method calculates the debit transfer function
 function[dict_p_tf] = calculate_debit_transfer_function(dict_debit_system)
+%% Retrieve required parameters;
+F0 = dict_debit_system('initial_debit');
+D = dict_debit_system('diameter');
+L = dict_debit_system('length');
+alpha = dict_debit_system('flow_coefficient');
+q = dict_debit_system('density');
+
+
+S = pi * D * L;
+V0 = pi * L * D^2 /4;
+
+%% Calculate the transfer function along with it's parameters
+dict_p_tf = containers.Map;
+
+dict_p_tf('amplification') = (alpha^2 * V0)/F0;
+dict_p_tf('time_constant') = (alpha^2 * S^2)/(F0 * q);
+
+s = tf('s');
+dict_p_tf('value') = dict_p_tf('amplification')/(dict_p_tf('time_constant')*s + 1);
 
 end
